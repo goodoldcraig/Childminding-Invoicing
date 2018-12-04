5ed306edc37ac0f6b11937271b10d826dfc4dd29
Craig Franklin <craig.s.f@hotmail.co.uk>
2018-12-04 21:13:10 +0000

    Add files via upload

---
+++ ManageMyChild/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManageMyChild.Startup))]
+namespace ManageMyChild
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
